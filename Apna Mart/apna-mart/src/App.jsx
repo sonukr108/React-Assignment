367732e8aa1282a7520555ca1f7dcbddf85d3d90
Sonu Kumar <sv808494@gmail.com>
2025-03-24 16:57:31 +0530

    add routing

--- Apna Mart/apna-mart/src/App.jsx
+++ Apna Mart/apna-mart/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { BrowserRouter, Router, Route } from 'react-router-dom'
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <BrowserRouter>
+      <Router>
+        <Route>
+          
+        </Route>
+      </Router>
+      </BrowserRouter>
     </div>
   )
 }
--- Apna Mart/apna-mart/src/components/Navbar.jsx
+++ Apna Mart/apna-mart/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import logo from '../assets/apnamartLogo.svg';
 const Navbar = () => {
   return (
-    <div className='h-[80px] w-screen bg-[#2F61AE] text-white'>
-      <h1 className='text-2xl font-bold'>Apna Mart</h1>
+    <div className='h-[80px] w-screen bg-[#2F61AE] text-white flex items-center px-5'>
+      <div>
+        <img src={logo} alt="Apna Mart Logo" className="h-5 w-auto" />
+      </div>
     </div>
   )
 }

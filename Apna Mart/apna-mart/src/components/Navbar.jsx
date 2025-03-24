c533d5b870ea37c0d7a394e8815cd95e001f2041
Sonu Kumar <sv808494@gmail.com>
2025-03-24 16:35:37 +0530

    creating new project

---
+++ Apna Mart/apna-mart/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='h-[80px] w-screen bg-[#2F61AE] text-white'>
+      <h1 className='text-2xl font-bold'>Apna Mart</h1>
+    </div>
+  )
+}
+
+export default Navbar

546e2c16d50805698ce7b2189981e8fbbc162dd9
Sonu Kumar <sv808494@gmail.com>
2025-03-24 20:32:08 +0530

    change in home page

--- Apna Mart/apna-mart/src/components/Navbar.jsx
+++ Apna Mart/apna-mart/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className='h-[80px] w-screen bg-[#2F61AE] text-white flex items-center px-[8%] gap-10 justify-between'>
-      <div>
+      <div className='flex items-center gap-5'>
       <Link to='/'><img src={logo} alt="Apna Mart Logo" className="h-5 w-auto" /></Link>
       <span className='flex gap-4'>
         <Link to='/'>Home</Link>
@@ -13,7 +13,7 @@ const Navbar = () => {
       </span>
       </div>
       <div className="buttons">
-        <button>Log in</button>
+        <button className='bg-red-600 text-white p-2 rounded-lg text-lg font-bold'>Log in</button>
       </div>
 
     </div>

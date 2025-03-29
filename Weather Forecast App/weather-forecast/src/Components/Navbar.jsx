edb856c7aa113f13b17004d4eb1218936b227db6
Sonu Kumar <sv808494@gmail.com>
2025-03-29 21:28:25 +0530

    chnage something in design

--- Weather Forecast App/weather-forecast/src/Components/Navbar.jsx
+++ Weather Forecast App/weather-forecast/src/Components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
             <h1 className='text-xl font-bold dark:text-white text-black'>Weather App</h1>
             <button
                 onClick={() => changeTheme(setDark)}
-                className='h-10 w-fit px-4 xl:px-6 bg-orange-800 hover:bg-orange-700 dark:bg-orange-500 text-white font-semibold rounded transition-all duration-300 cursor-pointer'>
+                className='h-10 w-fit px-4 xl:px-6 bg-orange-800 hover:bg-orange-700 dark:bg-orange-500 dark:text-white text-black font-semibold rounded transition-all duration-300 cursor-pointer'>
                 {dark ? "Light" : "Dark"}
             </button>
         </nav>

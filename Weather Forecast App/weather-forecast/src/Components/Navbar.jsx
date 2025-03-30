a85cfb06c647edb9e651b1be3dcc7c1a95543781
Sonu Kumar <sv808494@gmail.com>
2025-03-30 14:13:36 +0530

    Added dark mode on Wheather page and navabr

--- Weather Forecast App/weather-forecast/src/Components/Navbar.jsx
+++ Weather Forecast App/weather-forecast/src/Components/Navbar.jsx
@@ -9,11 +9,11 @@ const Navbar = () => {
         setDark(prev => !prev);
     }
     return (
-        <nav className='h-16 w-full bg-orange-500 dark:bg-orange-800 flex items-center justify-between px-5 py-5 xl:px-10'>
-            <h1 className='text-xl font-bold dark:text-white text-black'>Weather App</h1>
+        <nav className='h-16 w-full bg-none flex items-center justify-between px-5 py-5 xl:px-10'>
+            <h1 className='text-xl font-bold dark:text-white text-black'>Sonu's Weather App</h1>
             <button
                 onClick={() => changeTheme(setDark)}
-                className='h-10 w-fit px-4 xl:px-6 bg-orange-800 hover:bg-orange-700 dark:bg-orange-500 dark:text-white text-black font-semibold rounded transition-all duration-300 cursor-pointer'>
+                className='h-10 w-fit px-4 xl:px-6 bg-black hover:bg-gray-500 dark:bg-white dark:text-black text-white font-bold rounded transition-all duration-300 cursor-pointer'>
                 {dark ? "Light" : "Dark"}
             </button>
         </nav>
